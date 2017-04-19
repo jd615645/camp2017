83cb178837e86e05baad16142a10301cf5498bc9
jayhung97724 <jayhung97724@gmail.com>
2017-04-19 23:38:16 +0800

    Add message modal

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,17 @@ $(document).ready(function() {
   particlesJS.load('bg', '../json/particles.json', function() {
     console.log('callback - particles.js config loaded');
   });
+  // $('.modal').modal()
+  $('.detail').click(function() {
+    var which_detail = $(this).attr('value');
+    $('#' + which_detail).css('z-index', 1050);
+    $('#' + which_detail).modal('show');
+
+  });
+
+  $('.modal .close').click(function() {
+    var which_detail = $(this).attr('value');
+    $('#' + which_detail).css('z-index', 1050);
+    $('#' + which_detail).modal('hide');
+  });
 });
